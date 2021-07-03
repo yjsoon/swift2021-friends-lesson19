bacedf44b53618a7286da5ee0d3f432fa50d2542
YJ Soon <yjsoon@tinkertanker.com>
2021-07-03 09:14:37 +0800

    Initial Commit

---
+++ Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by YJ Soon on 3/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by YJ Soon on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
