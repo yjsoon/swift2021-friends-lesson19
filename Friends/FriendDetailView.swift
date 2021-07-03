3165dfafa3da2ad7d1a2b4f79d85b9fb89abe4b3
YJ Soon <yjsoon@tinkertanker.com>
2021-07-03 10:28:56 +0800

    Added details for Friend

--- Friends/FriendDetailView.swift
+++ Friends/FriendDetailView.swift
@@ -7,12 +7,37 @@
 
 import SwiftUI
 
+let heroPicHeight: CGFloat = 250
+let profilePicSize: CGFloat = 150
+
 struct FriendDetailView: View {
     
     var friend: Friend
     
     var body: some View {
-        Text(friend.name)
+        VStack(spacing: 0) {
+            Image(friend.slothImage)
+                .resizable()
+                .scaledToFill()
+                .frame(height: heroPicHeight)
+            
+            Image(friend.name)
+                .resizable()
+                .scaledToFill()
+                .frame(width: profilePicSize, height: profilePicSize)
+                .mask(Circle())
+                .overlay(Circle().stroke(lineWidth: 10).foregroundColor(.white))
+                .offset(x: 0, y: -profilePicSize/2)
+                .shadow(radius: 10)
+                .padding(.bottom, -profilePicSize/2 + 20)
+            
+            // Can also use HStack, or a Label
+            Text("\(Image(systemName: friend.icon)) \(friend.school)")
+                .font(.system(size: 24))
+                .padding()
+            
+            Spacer()
+        }
             .navigationTitle(friend.name)
     }
 }
