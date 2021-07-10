d07bad893c2c58f4dc8aeeba1e1ffdcd9b0f6d75
YJ Soon <yjsoon@tinkertanker.com>
2021-07-10 09:39:14 +0800

    Added in ScrollView

--- Friends/FriendDetailView.swift
+++ Friends/FriendDetailView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-let heroPicHeight: CGFloat = 250
+let heroPicHeight: CGFloat = 300
 let profilePicSize: CGFloat = 150
 
 struct FriendDetailView: View {
@@ -15,43 +15,44 @@ struct FriendDetailView: View {
     @Binding var friend: Friend
     
     var body: some View {
-        VStack(spacing: 0) {
-            Image(friend.slothImage)
-                .resizable()
-                .scaledToFill()
-                .frame(height: heroPicHeight)
-            
-            Image(friend.name)
-                .resizable()
-                .scaledToFill()
-                .frame(width: profilePicSize, height: profilePicSize)
-                .mask(Circle())
-                .overlay(Circle().stroke(lineWidth: 10).foregroundColor(.white))
-                .offset(x: 0, y: -profilePicSize/2)
-                .shadow(radius: 10)
-                .padding(.bottom, -profilePicSize/2 + 20)
-            
-            // Can also use HStack, or a Label
-            Text("\(Image(systemName: friend.icon)) \(friend.school)")
-                .font(.system(size: 24))
-                .padding()
-            
-            VStack(alignment: .leading) {
-                Text("Attack")
-                Slider(value: $friend.attack,
-                       in: 0...15,
-                       step: 1)
+        ScrollView {
+            VStack(spacing: 0) {
+                Image(friend.slothImage)
+                    .resizable()
+                    .frame(height: heroPicHeight)
                 
-                Text("Defence")
-                Slider(value: $friend.defence,
-                       in: 0...15,
-                       step: 1)
-            }
-            .padding()
+                Image(friend.name)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: profilePicSize, height: profilePicSize)
+                    .mask(Circle())
+                    .overlay(Circle().stroke(lineWidth: 10).foregroundColor(.white))
+                    .offset(x: 0, y: -profilePicSize/2)
+                    .shadow(radius: 10)
+                    .padding(.bottom, -profilePicSize/2 + 20)
+                
+                // Can also use HStack, or a Label
+                Text("\(Image(systemName: friend.icon)) \(friend.school)")
+                    .font(.system(size: 24))
+                    .padding()
+                
+                VStack(alignment: .leading) {
+                    Text("Attack")
+                    Slider(value: $friend.attack,
+                           in: 0...15,
+                           step: 1)
+                    
+                    Text("Defence")
+                    Slider(value: $friend.defence,
+                           in: 0...15,
+                           step: 1)
+                }
+                .padding()
 
-            Spacer()
-        }
+                Spacer()
+            }
             .navigationTitle(friend.name)
+        }
     }
 }
 
